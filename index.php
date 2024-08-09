0c89e37edb2df6820cf5c404044cd0342180a8ee
Joao Lorenco <jllorenc00@gmail.com>
2024-08-09 20:17:35 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade Posts</title>
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <button onclick="buscarPosts()">Buscar Post</button>
+    <table> 
+        <thead>
+            <tr>
+                <td>User ID</td>
+                <td>ID</td>
+                <td>Título</td>
+                <td>Mensagem</td>
+            </tr>
+        </thead>
+        <tbody id="tablebody">
+
+        </tbody>
+
+    </table>
+    
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,28 @@
+function buscarPosts(){
+    var url = "https://jsonplaceholder.typicode.com/posts";
+
+    var tablebody = document.querySelector("#tablebody");
+
+    fetch(`${url}`)
+        .then((response) => response.json())
+        .then((data) => {
+
+            console.log("mensagem");
+            console.log(data);
+
+            data.forEach(post => {
+                //post user.id post.title post.body
+                tablebody.innerHTML += '<tr>' 
+                            + '<td>' + post.userId + '</td>'
+                            + '<td>' + post.id + '</td>'
+                            + '<td>' + post.title + '</td>'
+                            + '<td>' + post.body + '</td>'
+                            + '</tr>';
+                    
+            });
+
+            
+        }).catch((error) => {
+            console.error("Deu erro na requisição" + error);
+        });
+}
